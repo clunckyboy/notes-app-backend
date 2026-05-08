4340cef41bc8d38ebdd8d3539492c86d4330e080
clunckyboy <edwinjonatanpurba@gmail.com>
2026-05-08 22:16:59 +0700

    minor typo fix

--- src/services/exports/routes/index.js
+++ src/services/exports/routes/index.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import { exportNotes } from '../controller/export-controller.js';
 import authenticateToken from '../../../middlewares/auth.js';
-import validate from '../../../middlewares/validate.js';
+import { validate } from '../../../middlewares/validate.js';
 import { exportPayloadSchema } from '../validator/schema.js';
 
 const router = Router();
 
-router.post('/exports/notes', authenticateToken, validate(exportPayloadSchema), exportNotes);
+router.post('/export/notes', authenticateToken, validate(exportPayloadSchema), exportNotes);
 
 export default router;
